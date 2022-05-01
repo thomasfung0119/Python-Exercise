f93c05f160036f103068a78bdf9125dbaa20ac50
thomasfung0119 <103992005+thomasfung0119@users.noreply.github.com>
2022-05-01 22:35:19 +0800

    Add files via upload

--- Progress Bar/Progress Bar.py
+++ Progress Bar/Progress Bar.py
@@ -1,9 +1,19 @@
-import colorama #pip install colorama
+import colorama
+import math
 
 def progress_bar(progress, total, load_color=colorama.Fore.YELLOW, end_color=colorama.Fore.GREEN):
 	percent = 100 * (float(progress) / float(total))
 	bar = "█" * int(percent) + "-" * (100 - int(percent))
-	print(load_color + f"\r|{bar}|{percent.2f}%", end="\r")
+	print(load_color + f"\r|{bar}|{percent:.2f}%", end="\r")
 	if (progress==total):
-	  print(end_color + f"\r|{bar}|{percent.2f}%", end="\r"
-	print(colorama.Fore.RESET)
+	    print(end_color + f"\r|{bar}|{percent:.2f}%", end="\r")
+
+
+numbers = [x * 5 for x in range(2000,3000)]
+result = []
+
+progress_bar(0,len(numbers))
+for i, x in enumerate(numbers):
+    result.append(math.factorial(x))
+    progress_bar(i+1,len(numbers))
+print(colorama.Fore.RESET + "\r")
